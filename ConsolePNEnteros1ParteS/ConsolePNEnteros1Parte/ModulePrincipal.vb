29b1345f9706be06a8c288bd8ae460b9c429efaf
jhasmany_jhunnior@outlook.com <jhasmany>
2020-05-05 09:59:24 -0400

    Actualisacion en la parte visual de la consola

---
+++ ConsolePNEnteros1ParteS/ConsolePNEnteros1Parte/ModulePrincipal.vb
@@ -0,0 +1,81 @@
+﻿
+Module ModulePrincipal
+
+    Sub Main()
+
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine("=============================================")
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("                 U.A.G.R.M.")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("               PROGRAMACION I")
+        Console.ForegroundColor = ConsoleColor.DarkCyan
+        Console.WriteLine("        PRACTICO DE NUMEROS ENTEROS")
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine("                 1RA PARTE")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(" =============================================")
+        Console.WriteLine("")
+        Console.ForegroundColor = ConsoleColor.Magenta
+        Console.WriteLine(" DOCENTE: Ing. Mollo")
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine("")
+        Console.WriteLine(" REALIZAR OPERACIONES PARA LA CLASE NUMEROS ENTEROS:")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine("---------------------------------------------------")
+        Console.WriteLine("")
+        Console.WriteLine(" ELIJA LA OPCION:")
+        Console.WriteLine("")
+
+        Console.WriteLine(" [1] - Verificar si un NE pertenece a la serie de Fibonacci")
+        Console.WriteLine(" [2] - Verificar si un NE. Pertenece a la sucesión regular (Serie regular)")
+        Console.WriteLine(" [3] - Accesar dígitos (de nd: # de dígitos) en sentido izquierda a derecha en una cadena/string")
+        Console.WriteLine(" [4] - Filtrar (dejar pasar) dígitos impares de un NE de a nd dígitos, el resultado en un string")
+        Console.WriteLine(" [5] - Filtrar (dejar pasar) dígitos primos de un NE de a nd dígitos, el resultado en un string")
+        Console.WriteLine(" [6] - Acumular con dígitos de Fibonacci")
+        Console.WriteLine(" [7] - Convertir un NE ( se supone en base 10) a base B (2<=B>=9)")
+        Console.WriteLine(" [8] - Con dígitos primos de nd dígitos, acumular: (Ver el ejercicio N° 4 como ayuda)")
+        Console.WriteLine(" [9] - Encontrar el digito mayor de ND dígitos")
+        Console.WriteLine(" [10] - Encontrar la frecuencia de DIG de a nd dígitos de un NE")
+        Console.WriteLine("------------------------------------------------------------------------------------------------")
+
+
+
+
+
+        Console.WriteLine("")
+
+        Dim opcion As String
+        Console.Write(" INGRESE LA OPCION: ")
+        opcion = Console.ReadLine()
+        Console.ResetColor()
+
+        Select Case opcion
+            Case "1"
+                Console.WriteLine("acceso 1")
+            Case "2"
+                Console.WriteLine("acceso 2")
+            Case "3"
+                Console.WriteLine("acceso 3")
+            Case "4"
+                Console.WriteLine("acceso 4")
+            Case "5"
+                Console.WriteLine("acceso 5")
+            Case "6"
+                Console.WriteLine("acceso 6")
+            Case "7"
+                Console.WriteLine("acceso 7")
+            Case "8"
+                Console.WriteLine("acceso 8")
+            Case "9"
+                Console.WriteLine("acceso 9")
+            Case "10"
+                Console.WriteLine("acceso 10")
+            Case Else
+                Console.WriteLine("acceso fail")
+
+        End Select
+        Console.ReadLine()
+    End Sub
+
+End Module
--- ConsolePNEnteros1ParteS/ConsolePNEnteros1Parte/NEnteros.vb
+++ ConsolePNEnteros1ParteS/ConsolePNEnteros1Parte/NEnteros.vb
@@ -9,5 +9,34 @@
     Function descargar()
         Return n
     End Function
+    '------------------------------------------------------------
+    '1. Verificar si un NE pertenece a la serie de Fibonacci.
+    '    Ejemplo: N=13 => true o N=15=> false Fibonacci: 0,1,1,2,3,5,8,13,21,34,…
+    Function ID_Fib()
+        Dim c, a, b As Integer
+        Dim be As Boolean
+        c = 0
+        a = -1
+        b = 1
+        Do
+            c = a + b
+            a = b
+            b = c
+        Loop Until c = n Or c > n
+        If c = n Then
+            be = True
+        Else
+            be = False
+        End If
+        Return be
+    End Function
+    '-------------------------------------------------------------
+    '2. Verificar si un NE. Pertenece a la sucesión regular (Serie regular)
+    'Vi=7, r=3 Serie: 7,10,13,16,19,…. La Fórmula de la serie regular: t=vi+(i-1)*r
+    'Dónde: Vi:Valor inicial; r:Razon; i:sucesión progresiva estándar 1,2,3,4,5… y t:termino.
+    'Ejemplo: N=16 y Vi=7, r=3 => true N=15 y Vi=7;r=3 =>false
 
+    '3. Accesar dígitos (de nd: # de dígitos) en sentido izquierda a derecha en una cadena/string.
+    'Ejemplos: N=123456 y ND=1 => S=”1,2,3,4,5,6,”
+    'N=123456 y ND=2 => S=”12,34,56”
 End Class
